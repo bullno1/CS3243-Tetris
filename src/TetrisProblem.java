ae44a6ab45193ad6c54d5b38e0ec65379b56ee11
bullno1 <thebullno1@gmail.com>
2014-03-31 09:03:14 +0800

    Play several games with each genes and take worst score as fitness

--- src/TetrisProblem.java
+++ src/TetrisProblem.java
@@ -1,9 +1,11 @@
+import java.util.ArrayList;
 import java.util.Random;
 import java.util.concurrent.ForkJoinPool;
 
 class TetrisProblem implements ProblemDomain<WeightSet> {
 	public TetrisProblem(ForkJoinPool forkJoinPool) {
 		this.forkJoinPool = forkJoinPool;
+		this.mapReduce = new PlayerSkeleton.MapReduce(forkJoinPool);
 	}
 
 	@Override
@@ -26,18 +28,16 @@ class TetrisProblem implements ProblemDomain<WeightSet> {
 				maxScore = score;
 			}
 		}
-		
-		return maxScore > 5.f;
+
+		return maxScore > 1.f;
 	}
 
 	@Override
 	public float evaluateFitness(WeightSet gene) {
-		State state = new State();
-		PlayerSkeleton player = new PlayerSkeleton(forkJoinPool, gene.getWeights());
-		while(!state.hasLost()) {
-			state.makeMove(player.pickMove(state, state.legalMoves()));
-		}
-		return (float)state.getRowsCleared();
+		final int NUM_GAMES = 5;
+		ArrayList<WeightSet> inputs = new ArrayList<WeightSet>(NUM_GAMES);
+		for(int i = 0; i < NUM_GAMES; ++i) { inputs.add(gene); }
+		return mapReduce.mapReduce(EVAL_FUNC, REDUCE_FUNC, inputs);
 	}
 
 	@Override
@@ -48,29 +48,57 @@ class TetrisProblem implements ProblemDomain<WeightSet> {
 	@Override
 	public WeightSet[] crossover(WeightSet parent1, WeightSet parent2,
 			int crossOverPoint) {
-		
+
 		float[] parent1Weights = parent1.getWeights();
 		float[] parent2Weights = parent2.getWeights();
 		int numChromosomes = parent1Weights.length;
-		
+
 		WeightSet children1 = newGene();
 		WeightSet children2 = newGene();
 		float[] weights1 = children1.getWeights();
 		float[] weights2 = children2.getWeights();
-		
+
 		System.arraycopy(parent1Weights, 0, weights1, 0, crossOverPoint);
 		System.arraycopy(parent2Weights, 0, weights2, 0, crossOverPoint);
 		System.arraycopy(parent1Weights, crossOverPoint, weights2, crossOverPoint, numChromosomes - crossOverPoint);
 		System.arraycopy(parent2Weights, crossOverPoint, weights1, crossOverPoint, numChromosomes - crossOverPoint);
-		
+
 		return new WeightSet[] { children1, children2 };
 	}
-	
+
 	private static WeightSet newGene() {
 		float[] weights = new float[PlayerSkeleton.EVALUATORS.length];
 		return new WeightSet(weights);
 	}
 
 	private ForkJoinPool forkJoinPool;
+	private PlayerSkeleton.MapReduce mapReduce;
 	private Random random = new Random();
+
+	private final PlayerSkeleton.MapFunc<WeightSet, Float> EVAL_FUNC = new PlayerSkeleton.MapFunc<WeightSet, Float>() {
+		@Override
+		public Float map(WeightSet gene) {
+			State state = new State();
+			PlayerSkeleton player = new PlayerSkeleton(forkJoinPool, gene.getWeights());
+			while(!state.hasLost()) {
+				state.makeMove(player.pickMove(state, state.legalMoves()));
+			}
+			return (float)state.getRowsCleared();
+		}
+	};
+
+	private final PlayerSkeleton.ReduceFunc<Float, Float> REDUCE_FUNC = new PlayerSkeleton.ReduceFunc<Float, Float>() {
+		@Override
+		public Float reduce(Iterable<Float> inputs) {
+			Float worseScore = Float.MAX_VALUE;
+
+			for(Float score: inputs) {
+				if(score.compareTo(worseScore) < 0) {
+					worseScore = score;
+				}
+			}
+
+			return worseScore;
+		}
+	};
 }
